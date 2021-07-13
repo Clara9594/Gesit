af934a5a8f786c9b7c752f9591ef53988e438922
alizahanum <alizahanum5@gmail.com>
2021-07-13 22:40:58 +0700

    newest

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,32 @@
 <template>
-<v-main class="courses">
-   <v-card background-color="pink" max-width="1000" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+<v-main class="homepage">
+   <v-card class="card" tile color="FFC0CB" elevation="3" outlined>
       <v-list-item-title style="font-weight:bolder; font-size:x-large;">Welcome, ACM</v-list-item-title>
      <v-list-item-title>Have a nice day at work!</v-list-item-title>
-     
-    </v-card>
+  </v-card>
+
   <v-list-item-title style="font-weight:bolder; font-size:x-large;">Project</v-list-item-title>
-  <v-card max-width="400" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+  <v-card class="ml-9 mr-9 mb-5" max-width="500" elevation="3" outlined>
       <v-row class="mt-3">
         <v-col sm="5" class="ml-10">
          <img class="mt-8 ml-3" height="150px" src="../assets/gov.png">
-          <p class="text-left font-weight-bold font-size-x-large">Governance Project</p>
+          <p class="text-subtitle-2 text-center font-weight-bold font-size-x-large">Governance Project</p>
         </v-col>
       </v-row>
       <br>
      
        <v-btn
-      class = "d-flex justify-center mb-6"
+      class = "mr-10"
       rounded
       color = "#FFA500"
-      dark
-    >
+      align="center"
+      block
+      dark>
       Choose
     </v-btn>
     </v-card>
-
-      <v-card max-width="400" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+    
+      <v-card class="ml-9 mr-9 mb-5" max-width="500" elevation="3" outlined>
       <v-row class="mt-3">
         <v-col sm="5" class="ml-10">
          <img class="mt-8 ml-3" height="150px" src="../assets/audit.png">
@@ -42,6 +43,7 @@
       Choose
     </v-btn>
     </v-card>
+
  </v-main>
 </template>
 
@@ -66,5 +68,10 @@ methods: {
 </script>
 
 <style scope>
+.card{
+width:"relative";
+position:"absolute"
+}
+
 
 </style>
