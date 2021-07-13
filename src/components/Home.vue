b98aa15ee70d132ea6b4762eaae6e6a6772e76a6
alizahanum <alizahanum5@gmail.com>
2021-07-13 19:29:38 +0700

    report

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,48 @@
 <template>
-<v-main class="courses"> 
- <v-toolbar-title class="text-left font-weight-bold ml-9  mb-6">Courses</v-toolbar-title> 
-  <v-card max-width="500" class="ml-9 mr-9 mb-5" elevation="3" outlined> 
-      <v-row class="mt-3"> 
-        <v-col sm="5" class="ml-10"> 
-         <img class="mt-8 ml-3" height="150px" src="../assets/checklist-removebg-preview.png"> 
+<v-main class="courses">
+   <v-card background-color="pink" max-width="1000" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+      <v-list-item-title style="font-weight:bolder; font-size:x-large;">Welcome, ACM</v-list-item-title>
+     <v-list-item-title>Have a nice day at work!</v-list-item-title>
+     
+    </v-card>
+  <v-list-item-title style="font-weight:bolder; font-size:x-large;">Project</v-list-item-title>
+  <v-card max-width="400" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+      <v-row class="mt-3">
+        <v-col sm="5" class="ml-10">
+         <img class="mt-8 ml-3" height="150px" src="../assets/gov.png">
+          <p class="text-left font-weight-bold font-size-x-large">Governance Project</p>
         </v-col>
-        <v-col cols="15" sm="100" md="15" lg="90" class="ml-20"> 
-          <p class="text-left font-weight-bold">Governance Project</p> 
-        </v-col> 
-      </v-row> 
-      <br> 
-      <v-btn class="d-flex justify-center mb-6" color = "#FFA500"> 
-            Choose 
-    </v-btn>   
-    </v-card> 
+      </v-row>
+      <br>
+     
+       <v-btn
+      class = "d-flex justify-center mb-6"
+      rounded
+      color = "#FFA500"
+      dark
+    >
+      Choose
+    </v-btn>
+    </v-card>
 
-    <v-card max-width="500" class="ml-9 mr-9 mb-5" elevation="3" outlined> 
-      <v-row class="mt-3"> 
-        <v-col sm="5" class="ml-10"> 
-         <img class="mt-8 ml-3" height="150px" src="../assets/checklist-removebg-preview.png"> 
+      <v-card max-width="400" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+      <v-row class="mt-3">
+        <v-col sm="5" class="ml-10">
+         <img class="mt-8 ml-3" height="150px" src="../assets/audit.png">
+          <p class="text-left font-weight-bold">Temuan Audit</p>
         </v-col>
-        <v-col cols="15" sm="100" md="15" lg="90" class="ml-20"> 
-          <p class="text-left font-weight-bold">Temuan Audit</p> 
-        </v-col> 
-      </v-row> 
-      <br> 
-      <v-btn  class="d-flex justify-center mb-6" color = "#FFA500"> 
-            Choose 
-    </v-btn>   
-    </v-card> 
+      </v-row>
+      <br>
+      <v-btn
+      class = "d-flex justify-center mb-6"
+      rounded
+      color = "#FFA500"
+      dark
+    >
+      Choose
+    </v-btn>
+    </v-card>
  </v-main>
-
 </template>
 
 <script>
--- src/components/Reporting.vue
+++ src/components/Reporting.vue
@@ -1,13 +1,27 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-layout align-center justify-center>
-            <p>Ini Konten Reporting nya disini!</p>
-        </v-layout>
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-main class="courses">
+    <v-toolbar-title class="text-left font-weight-bold ml-9  mb-6">Laporan Rencana Pengembangan Project IT</v-toolbar-title>
+    <v-card max-width="1600" class="ml-9 mr-9 mb-5" elevation="3" outlined>
+      <v-row class="mt-3">
+        <v-col sm="5" class="ml-10">
+          <p class="text-left font-weight-bold">Type of Report</p>
+           <v-col
+        dense
+        class="d-flex"
+        cols="100"
+        sm="6"
+        >
+        <v-select
+          :items="items"
+          label="Report"
+          outlined
+        ></v-select>
+      </v-col>
+        </v-col>
+      </v-row>
+      <br>
+    </v-card>
+  </v-main>
 </template>
 
 <script>
