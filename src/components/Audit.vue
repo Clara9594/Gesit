2fe87c6093db0a9bc883dc9356dd0c75ecf41dde
bela <sintabelaaa@gmail.com>
2021-07-15 10:58:10 +0700

    pull terbaru

--- src/components/Audit.vue
+++ src/components/Audit.vue
@@ -2,66 +2,53 @@
 
 <v-main class="audit">
 <v-container fluid>
-
-    <h1 class="page-title mt-10 mb-6">Audit</h1>
+<br>
+    <h1 class="page-title mt-10 mb-6 teal--text">Audit</h1>
     <v-row>
       <v-col>
-        <v-card class="mx-1 mb-1">
+        <v-card class="mx-1 mb-1" style="justify-content: center;">
           <v-card-title class="pa-6 pb-3">
-          <img src="../assets/gov.png">
+          <img src="../assets/gov.png" height="100px">
           <br>
-            <p>Input tidak lanjut</p>
           </v-card-title>
+           <p style="font-size:x-large; font-weight:bolder; text-align:center;" class="teal--text">Governance Project</p>
            <v-btn
-            class = "d-flex justify-center mb-6"
             rounded
-            color = "#FFA500"
+            color = "#F15A23"
             dark
+            :style="{left: '50%', transform:'translateX(-50%)'}"
           >
           Choose
          </v-btn>
-         <br>
-          <!-- <v-card-text class="pa-6 pt-0">
+          <v-card-text class="pa-6 pt-0">
             <v-row no-gutters class="typography-widget pb-6">
               <v-col cols="12" class="card-dark-grey">
-                <h1>h1. Heading</h1>
-                <h2>h2. Heading</h2>
-                <h3>h3. Heading</h3>
-                <h4>h4. Heading</h4>
-                <h5>h5. Heading</h5>
-                <h6>h6. Heading</h6>
               </v-col>
             </v-row>
-          </v-card-text> -->
+          </v-card-text>
         </v-card>
       </v-col>
       <v-col>
         <v-card class="mx-1 mb-1">
           <v-card-title class="pa-6 pb-3">
-            <img src="../assets/audit.png">
-            <p>Upload RHA</p>
+            <img src="../assets/audit.png" align="right" height="100px">
           </v-card-title>
+          <p style="font-size:x-large; font-weight:bolder; text-align:center;" class="teal--text">Temuan Audit</p>
            <v-btn
-            class = "d-flex justify-center mb-6"
             rounded
-            color = "#FFA500"
+            color = "#F15A23"
             dark
+            :style="{left: '50%', transform:'translateX(-50%)'}"
+            link to= "/audit"
           >
            Choose
         </v-btn>
-        <br>
-          <!-- <v-card-text class="pa-6 pt-0">
+          <v-card-text class="pa-6 pt-0">
             <v-row no-gutters class="typography-widget pb-6">
               <v-col cols="12" class="card-dark-grey">
-                <h1 class="primary--text">h1. Heading</h1>
-                <h2 class="success--text">h2. Heading</h2>
-                <h3 class="secondary--text">h3. Heading</h3>
-                <h4 class="warning--text">h4. Heading</h4>
-                <h5 class="primary--text">h5. Heading</h5>
-                <h6 class="info--text">h6. Heading</h6>
               </v-col>
             </v-row>
-          </v-card-text> -->
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -90,6 +77,13 @@ methods: {
 </script>
 
 <style scope>
-
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.homepage{
+  padding:10px;
+}
                                                                                                                                                             
 </style>
