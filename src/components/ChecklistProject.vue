2a583f997c5298207f8f0bd29676c2bcabc2cfcb
alizahanum <alizahanum5@gmail.com>
2021-07-21 19:01:13 +0700

    approved

--- src/components/ChecklistProject.vue
+++ src/components/ChecklistProject.vue
@@ -33,10 +33,40 @@
       color = "#005E6A"
       dark
       :style="{left: '50%', transform:'translateX(-50%)'}"
-      link to="/checklist"
+      @click = "dialog = true" :right="true" :absolute="true"
       >
       Submit
   </v-btn>
+  <v-dialog v-model = "dialog" persistent max-width = "600px">
+        <v-card>
+        <v-toolbar flat dense> 
+        <v-spacer></v-spacer> 
+        <v-toolbar-items dense> 
+        <v-btn icon @click="dialog = false"> 
+        <v-icon>mdi-close-circle</v-icon> 
+        </v-btn> 
+        </v-toolbar-items> 
+        </v-toolbar>
+          <v-card flat class="pl-9 pr-9 mt-16">
+              <h1>APPROVED!</h1>
+              <p class="text3">Files succesfully uploaded</p>
+              <v-flex class="px-10 pt-5 pb-2 text-center">
+              <img id="pic" src="../assets/checked 1.png"></v-flex>
+              <h4 class="text3" style="font-weight:bolder; font-size:xx-large; justify-content:center">6/12</h4>
+          </v-card>
+        <br>
+      <v-card-actions class="mr-8" style="justify-content:center" >
+          <v-btn outlined color = "#005E6A" dark link to="/home">
+              Back
+          </v-btn>
+          <v-btn color = "#005E6A" dark>
+              Reminder
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  
+
 </v-main>
 </v-app>
 </template>
@@ -52,8 +82,9 @@ created () {
 },
 data() {
     return {
+      dialog: false,
       singleSelect: false,
-        selected: [],
+      selected: [],
         headers: [
           {
             text: 'Dessert (100g serving)',
@@ -88,7 +119,10 @@ data() {
 methods: {
     back(){
     this.$router.back();
-  }
+  },
+  close() {
+      this.dialog = false;
+    },
 }
 };
 </script>
@@ -98,9 +132,27 @@ methods: {
   color:#005E6A;
   font-size:x-large; font-weight:bolder; text-align:center;
 }
-
 .text2{
 text-align: center;
+}
+.text3{
+  color: #F15A23;
+  text-align: center;
+}
+.kotak{
+  height:70px;
+}
+#pic{
+height:110px;
+align-content: center;
+}
+#icon{
+  align-content: right;
+}
+h1{
+  color:#005E6A;
+  text-align: center;
+  
 }
 
                                                                                                                                                             
