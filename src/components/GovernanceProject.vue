4d8c1071b880785fb55e3777ffab0e5cdaf8e79c
alizahanum <alizahanum5@gmail.com>
2021-07-26 15:35:56 +0700

    test

--- src/components/GovernanceProject.vue
+++ src/components/GovernanceProject.vue
@@ -5,53 +5,74 @@
       <v-btn class="ml-1 mr-3" outlined fab color="#005E6A" @click="back">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-    <h2 class="text text-center mt-5 mb-2" style="font-size:xx-large;">PROJECT PLANNING</h2>
-    <p class="text--secondary text-center">Choose the Project Planning what you want</p>
     </v-toolbar-title>
-<v-layout justify-center class="mb-16">
-        <v-card width="70%" class="mx-5 pl-10 pt-10 pr-10 pb-5">
+    <h2 class="text text-center mb-2 mt-5 px-5" style="font-size:xx-large;">PROJECT PLANNING</h2>
+    <p class="text--secondary text-center px-5">Choose the Project Planning what you want</p>
+<v-layout justify-center>
+        <v-card width="700px" elevation="2" class="px-3 mx-5 py-3 mt-3" outlined>
         <v-row>
-            <v-select class="mr-4"
+          <v-col cols="7" sm="9" md="9" class="pb-0">
+            <v-select class="mr-3"
             :items="items"
             label="IT Planning Session"
             outlined
+            dense
             ></v-select>
-            <v-btn class="mt-3"
+          </v-col>
+          <v-col cols="5" sm="3" md="3" class="pl-0 pb-0">
+            <v-btn
             color = "#F15A23"
             dark>Download</v-btn>
+          </v-col>
         </v-row>
         <v-row>
-            <v-select class="mr-4"
+          <v-col cols="7" sm="9" md="9" class="pb-0">
+            <v-select class="mr-3"
             :items="items"
             label="RPTI"
+            dense
             outlined
             ></v-select>
-            <v-btn class="mt-3"
+          </v-col>
+          <v-col cols="5" sm="3" md="3" class="pl-0 pb-0">
+            <v-btn
             color = "#F15A23"
             dark>Download</v-btn>
+          </v-col>
         </v-row>
         <v-row>
-            <v-select class="mr-4"
+          <v-col cols="7" sm="9" md="9" class="pb-0">
+            <v-select class="mr-3"
             :items="items"
             label="RBB"
+            dense
             outlined
             ></v-select>
-            <v-btn class="mt-3"
-            color = "#F15A23"
-            dark>Download</v-btn></v-row>
-        <v-row>
-        <v-select class="mr-4"
-            :items="items"
-            label="Insertion"
-            outlined
-            ></v-select>
-            <v-btn class="mt-3"
+          </v-col>
+          <v-col cols="5" sm="3" md="3" class="pl-0 pb-0">
+            <v-btn
             color = "#F15A23"
             dark>Download</v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="7" sm="9" md="9" class="pb-0">
+            <v-select class="mr-3"
+              :items="items"
+              label="Insertion"
+              dense
+              outlined
+              ></v-select>
+          </v-col>
+          <v-col cols="5" sm="3" md="3" class="pl-0 pb-0">
+              <v-btn
+              color = "#F15A23"
+              dark>Download</v-btn>
+          </v-col>
         </v-row>
         </v-card>
       </v-layout>
-      <v-layout justify-center class="mb-10">
+      <v-layout justify-center class="mt-10 mb-10">
       <v-btn 
           color = "#005E6A"
           dark
@@ -67,6 +88,9 @@
 
 <script>
   export default {
+    created () {
+      document.title = "Project Planning";
+    },
     data: () => ({
       items: ['Pertamina', 'BPJS'],
     }),
